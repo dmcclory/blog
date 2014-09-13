0f88d4f1aa720ac470aeab38c99f2691cb171dc5
Dan McClory <danmcclory@gmail.com>
2014-09-13 17:05:48 -0400

    refactor the markdown rendering & file lookup into a Blog class

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
 require 'redcarpet'
 
+class Blog
+  def initialize(articles_dir = nil)
+    @articles_dir = articles_dir || "articles"
+  end
+
+  def markdown_for(article_id)
+    article_path = File.join(@articles_dir, article_id + ".md")
+    metadata, text = File.read(article_path).split(/---/)[1..2]
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    markdown.render(text)
+  end
+end
+
 get '/' do
   erb :index
 end
@@ -13,9 +26,8 @@ get '/topics' do
   erb :topics
 end
 
-get '/blog/:article' do |article|
-  metadata, text = File.read("articles/" + article + '.md').split(/---/)[1..2]
-  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
-  @contents = "<h3>" + markdown.render(text) + "</h3>"
+get '/blog/:article' do |article_id|
+  blog = Blog.new
+  @contents = "<h3>" + blog.markdown_for(article_id)+ "</h3>"
   erb :article
 end
