5ae777e31e5f931476f7195a8b1600d47d1e802a
Dan McClory <danmcclory@gmail.com>
2014-09-14 15:39:24 -0400

    chotto refactor

--- app.rb
+++ app.rb
@@ -5,6 +5,17 @@ require 'redcarpet'
 class Blog
   def initialize(articles_dir = nil)
     @articles_dir = articles_dir || "articles"
+    @articles = load_articles
+  end
+
+  def load_articles
+    articles = []
+    Dir[@articles_dir + "/*.md"].each do |article_path|
+      metadata = Psych.load(File.read(article_path).split(/---/)[1])
+      metadata["file"] = File.basename(article_path, ".md")
+      articles << metadata
+    end
+    articles
   end
 
   def markdown_for(article_id)
@@ -15,24 +26,11 @@ class Blog
   end
 
   def sorted_by_topic
-    articles = []
-    Dir[@articles_dir + "/*.md"].each do |article_path|
-      metadata = Psych.load(File.read(article_path).split(/---/)[1])
-      metadata["file"] = File.basename(article_path, ".md")
-      articles << metadata
-    end
-    articles.group_by { |article| article["category"] || "random" }
+    @articles.group_by { |article| article["category"] || "random" }
   end
 
-  def sorted_by_date
-    articles = []
-    Dir[@articles_dir + "/*.md"].each do |article_path|
-      metadata = Psych.load(File.read(article_path).split(/---/)[1])
-      metadata["file"] = File.basename(article_path, ".md")
-      articles << metadata
-    end
-    puts articles.inspect
-    articles.group_by { |article| Date.parse("#{article['date'].year}-#{article['date'].month}-01")}.sort { |a, b| b[0] <=> a[0] }
+  def sorted_by_month
+    @articles.group_by { |article| Date.parse("#{article['date'].year}-#{article['date'].month}-01")}.sort { |a, b| b[0] <=> a[0] }
   end
 end
 
@@ -44,7 +42,7 @@ end
 
 get '/chron' do
   blog = Blog.new
-  @articles = blog.sorted_by_date
+  @articles = blog.sorted_by_month
   erb :chron
 end
 
