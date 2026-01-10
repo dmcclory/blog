4311f0aa0a43ccb464818de36e40a8e549b42ec2
Dan McClory <danmcclory@fastmail.com>
2026-01-10 12:11:51 -0500

    add a font w/ the lume google font plugin

--- _config.ts
+++ _config.ts
@@ -1,7 +1,13 @@
 import lume from "lume/mod.ts";
+import googleFonts from "lume/plugins/google_fonts.ts";
 
 const site = lume();
 
 site.add("/styles.css");
+
+site.use(googleFonts({
+  cssFile: "styles.css",
+  fonts: "https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap"
+}));
 
 export default site;
